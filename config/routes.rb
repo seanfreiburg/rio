92d4d11484c8af2e70ab87c5357ef5bae9ded3e8
Sean Freiburg <seanfreiburg@gmail.com>
2015-02-02 22:30:36 -0800

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  get 'about' => 'static_pages/about'
+
+  get 'contact' => 'static_pages/contact'
+
+  get 'pricing' => 'static_pages/pricing'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
